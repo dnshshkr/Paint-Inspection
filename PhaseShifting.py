362f60fdbe4a4fd5d4a1b2751066fd592c71ce92
dnshshkr <danish.shukor@live.iium.edu.my>
2023-01-12 11:01:48 +0800

    modify

--- PhaseShifting.py
+++ PhaseShifting.py
@@ -1,7 +1,7 @@
 """
 Capture projection pattern and decode x-coorde.
 """
-import cv2
+import cv2 as cv
 import numpy as np
 import structuredlight as sl
 import screeninfo
@@ -20,27 +20,27 @@ imageXY_name='imageXY_PhaseShifting'+specified
 
 def imshowAndCapture(cap, img_pattern, delay=350):
     window_name='projector'
-    cv2.imshow(window_name, img_pattern)
-    cv2.waitKey(delay)
+    cv.imshow(window_name, img_pattern)
+    cv.waitKey(delay)
     img_gray=cap.capture()
-    cv2.namedWindow("img_gray", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("img_gray", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow("img_gray", img_gray)
-    cv2.moveWindow("img_gray",0,0)
+    cv.namedWindow("img_gray", cv.WND_PROP_FULLSCREEN)
+    cv.setWindowProperty("img_gray", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+    cv.imshow("img_gray", img_gray)
+    cv.moveWindow("img_gray",0,0)
     return img_gray
 
 def main():
     # get the size of the screen
     try:
-        screen = screeninfo.get_monitors()[screen_id]
-        width, height = screen.width, screen.height
+        screen=screeninfo.get_monitors()[screen_id]
+        width,height=screen.width, screen.height
     except:
         print('Monitor not detected')
         exit()
     try:
         cap=basler.Basler(image_size=(1920,1080),rotate_image=180)
     except:
-        cap = cv2.VideoCapture(1) # External web camera
+        cap=cv.VideoCapture(1) # External web camera
         cap.open
     num:int=5
     F:int=35
@@ -48,29 +48,34 @@ def main():
     F1=F
     F2=int(float(F)*9.0/16.0)
 
-    phaseshiftingX = sl.PhaseShifting(num1,F1)
-    imlist_posi_x_pat = phaseshiftingX.generate((width, height))
-    phaseshiftingY = sl.PhaseShifting(num2,F2)
+    #generate x pattern
+    phaseshiftingX=sl.PhaseShifting(num1,F1)
+    imlist_posi_x_pat=phaseshiftingX.generate((width,height))
+    #imlist_nega_x_pat=[cv.rotate(img,cv.ROTATE_180) for img in imlist_posi_x_pat]
+    #imlist_posi_x_pat.extend(imlist_nega_x_pat)
 
-    # Generate and Decode y-coord
-    imlist = phaseshiftingY.generate((width, height))
-    imlist_posi_y_pat = sl.transpose(imlist)
+    #generate y pattern
+    phaseshiftingY=sl.PhaseShifting(num2,F2)
+    imlist=phaseshiftingY.generate((width, height))
+    imlist_posi_y_pat=sl.transpose(imlist)
+    #imlist_nega_y_pat=[cv.rotate(img,cv.ROTATE_180) for img in imlist_posi_y_pat]
+    #imlist_posi_y_pat.extend(imlist_nega_y_pat)
 
     #pattern projection screen
     window_name='projector'
-    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-    cv2.moveWindow(window_name, screen.x - 1, screen.y - 1)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv.namedWindow(window_name,cv.WND_PROP_FULLSCREEN)
+    cv.moveWindow(window_name,screen.x-1,screen.y-1)
+    cv.setWindowProperty(window_name,cv.WND_PROP_FULLSCREEN,cv.WINDOW_FULLSCREEN)
 
     # Capture
-    imlist_posi_x_cap = [imshowAndCapture(cap, img) for img in imlist_posi_x_pat]
+    imlist_posi_x_cap=[imshowAndCapture(cap, img) for img in imlist_posi_x_pat]
 
     # DecodeX
-    img_index_x = sl.PhaseShifting().decodeAmplitude(imlist_posi_x_cap)
+    img_index_x=sl.PhaseShifting().decodeAmplitude(imlist_posi_x_cap)
     plt.imsave(imageX_name+'.png',img_index_x,cmap='gray')
 
     # Capture
-    imlist_posi_y_cap = [imshowAndCapture(cap, img) for img in imlist_posi_y_pat]
+    imlist_posi_y_cap=[imshowAndCapture(cap, img) for img in imlist_posi_y_pat]
 
     # DecodeY
     img_index_y = sl.PhaseShifting().decodeAmplitude(imlist_posi_y_cap)
@@ -83,13 +88,14 @@ def main():
     del num,num1,num2,F,F1,F2,imlist,imlist_posi_x_pat,imlist_posi_x_cap,imlist_posi_y_pat,imlist_posi_y_cap
 
     # Visualize decode result
-    img_correspondence = cv2.merge([0.0*np.zeros_like(img_index_x), img_index_x/width, img_index_y/height])
+    img_correspondence = cv.merge([0.0*np.zeros_like(img_index_x), img_index_x/width, img_index_y/height])
     del img_index_x,img_index_y
     
-    img_correspondence = np.clip(img_correspondence*255.0, 0, 255).astype(np.uint8)
-    img_correspondence = cv2.cvtColor(img_correspondence, cv2.COLOR_BGR2GRAY)
+    img_correspondence=np.clip(img_correspondence*255.0, 0, 255).astype(np.uint8)
+    img_correspondence=cv.cvtColor(img_correspondence, cv.COLOR_BGR2GRAY)
     plt.imsave(imageXY_name+'.png',img_correspondence,cmap='gray')
-    cv2.destroyAllWindows()
+    #cv.imwrite(imageXY_name+'.png',img_correspondence)
+    cv.destroyAllWindows()
 
 if __name__=="__main__":
     main()
--- imageprocessing.py
+++ imageprocessing.py
@@ -1,14 +1,10 @@
 import cv2 as cv
 import numpy as np
-from PIL import ImageFilter
-from PIL import Image
 import os
 import PhaseShifting
 use_camera=0
 use_image=1
-load_PhaseShifting=1
-load_Gray=0
-path=r'image processing\\'
+path=r'image processing\white\\'
 for file_name in os.listdir(path):
     if os.path.isfile(path+file_name):
         os.remove(path+file_name)
@@ -17,52 +13,51 @@ if use_camera:
     imageY_name=PhaseShifting.imageY_name
     imageXY_name=PhaseShifting.imageXY_name
 elif use_image:
-    if load_PhaseShifting:
-        last_name='PhaseShifting'
-    elif load_Gray:
-        last_name='Gray'
+    last_name='PhaseShifting_white'
     imageX_name='imageX_'+last_name
     imageY_name='imageY_'+last_name
     imageXY_name='imageXY_'+last_name
 caption_size=1
 imgX=cv.imread(imageX_name+'.png',cv.IMREAD_COLOR)
-cv.putText(imgX,'imageX',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
 imgY=cv.imread(imageY_name+'.png',cv.IMREAD_COLOR)
-cv.putText(imgY,'imageY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-imgXY=cv.imread(imageXY_name+'.png',cv.IMREAD_GRAYSCALE)
-saveXY=cv.cvtColor(imgXY,cv.COLOR_GRAY2BGR)
-cv.putText(saveXY,'imageXY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-final_result=cv.cvtColor(imgXY,cv.COLOR_GRAY2BGR)
-cv.imwrite('image processing/1. imageX.png',imgX)
-cv.imwrite('image processing/2. imageY.png',imgY)
-cv.imwrite('image processing/3. imageXY.png',saveXY)
-result=imgXY
+imgXY=cv.imread(imageXY_name+'.png',cv.IMREAD_COLOR)
 
-global result_filter2D
-global result_medianBlur
-global result_erode
-global result_dilate
-global result_threshold
-global result_Canny
+final_result=imgX
+result=cv.cvtColor(final_result,cv.COLOR_BGR2GRAY)
+
+cv.putText(imgX,'imageX',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+cv.putText(imgY,'imageY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+cv.putText(imgXY,'imageXY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+cv.imwrite(path+'1. imageX.png',imgX)
+cv.imwrite(path+'2. imageY.png',imgY)
+cv.imwrite(path+'3. imageXY.png',imgXY)
 
 def main():
     global result
-    step={1:smoothen_image,
-          2:sharpen_image,
-          3:erode_image,
-          4:dilate_image,
-          5:convert_binary,
-          6:detect_edge}
+    process={1:smoothen_image,
+             2:sharpen_image,
+             3:erode_image,
+             4:dilate_image,
+             5:convert_binary,
+             6:detect_edge}
     
-    result=step[1](result,4,0)
-    result=step[3](result,5,0)
-    result=step[4](result,6,0)
-    result=step[2](result,7,0)
-    result=step[5](result,8,1)
-    result=step[6](result,9,1)
+    stitch_image_array=[]
+    result,stitch=process[1](result,4,1)
+    stitch_image_array.append(stitch)
+    result,stitch=process[3](result,5,1)
+    stitch_image_array.append(stitch)
+    result,stitch=process[4](result,6,1)
+    stitch_image_array.append(stitch)
+    result,stitch=process[2](result,7,0)
+    stitch_image_array.append(stitch)
+    result,stitch=process[5](result,8,1)
+    stitch_image_array.append(stitch)
+    result,stitch=process[6](result,9,1)
+    stitch_image_array.append(stitch)
 
     #find contours
-    contours=cv.findContours(result.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)[0]
+    contours=cv.findContours(result.copy(),cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)[0]
+    contourImage=np.zeros((len(final_result),len(final_result[0]),3),np.uint8)
     count_all=0
     perimeter_min=15.0
     perimeter_max=200.0
@@ -75,31 +70,35 @@ def main():
         #area=cv.contourArea(contour)
         if count_all:
             cv.rectangle(final_result,(x,y),(x+w,y+h),(b,g,r),2)
-            cv.putText(final_result,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+            cv.putText(final_result,"p{:.1f}".format(perimeter),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+            cv.drawContours(contourImage,[contour],0,(b,g,r),1)
+            cv.putText(contourImage,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
         elif (perimeter>=perimeter_min and perimeter<=perimeter_max) and (x!=prevx or y!=prevy):
             cv.rectangle(final_result,(x,y),(x+w,y+h),(b,g,r),2)
-            cv.putText(final_result,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+            cv.putText(final_result,"p{:.1f}".format(perimeter),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
             prevx,prevy=x,y
-            #print('prevx: {}, prevy: {}'.format(prevx,prevy))
+            cv.drawContours(contourImage,[contour],0,(b,g,r),1)
+            cv.putText(contourImage,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+    cv.putText(contourImage,'contours',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.imwrite(path+'10. contours.png',contourImage)
     cv.putText(final_result,'final result',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/10. final result.png',final_result)
+    cv.imwrite(path+'11. final result.png',final_result)
 
-    stitch_r0=np.concatenate((imgX,imgY,saveXY),axis=1)
-    stitch_r1=np.concatenate((result_medianBlur,result_erode,result_dilate),axis=1)
-    stitch_r2=np.concatenate((result_threshold,result_Canny,final_result),axis=1)
-    stitch=np.concatenate((stitch_r0,stitch_r1,stitch_r2),axis=0)
-    cv.imwrite('image processing/stitch.png',stitch)
+    stitch_r0=np.concatenate((imgX,imgY,imgXY),axis=1)
+    stitch_r1=np.concatenate((stitch_image_array[0],stitch_image_array[1],stitch_image_array[2]),axis=1)
+    stitch_r2=np.concatenate((stitch_image_array[3],stitch_image_array[4],stitch_image_array[5]),axis=1)
+    stitch_r3=np.concatenate((contourImage,final_result,np.zeros((1080,1920,3),np.uint8)),axis=1)
+    stitch=np.concatenate((stitch_r0,stitch_r1,stitch_r2,stitch_r3),axis=0)
+    cv.imwrite(path+'stitch.png',stitch)
 
 def smoothen_image(result,step,enable):#filter noise
-    global result_medianBlur
     result=cv.medianBlur(result,3) if enable else result
     result_medianBlur=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
     cv.putText(result_medianBlur,'medianBlur enabled' if enable else 'medianBlur disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/'+str(step)+'. medianBlur.png',result_medianBlur)
-    return result
+    cv.imwrite(path+str(step)+'. medianBlur.png',result_medianBlur)
+    return result,result_medianBlur
 
 def sharpen_image(result,step,enable):#image sharpening
-    global result_filter2D
     kernel=np.array([[-1,-1,-1],
                     [-1,9,-1],
                     [-1,-1,-1]])
@@ -112,42 +111,38 @@ def sharpen_image(result,step,enable):#image sharpening
 
     result_filter2D=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
     cv.putText(result_filter2D,'filter2D enabled' if enable else 'filter2D disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/'+str(step)+'. filter2D.png',result_filter2D)
-    return result
+    cv.imwrite(path+str(step)+'. filter2D.png',result_filter2D)
+    return result,result_filter2D
 
 def erode_image(result,step,enable):#erode
-    global result_erode
-    result=cv.erode(result,np.ones((3,3),np.uint8)) if enable else result
+    result=cv.erode(result,np.ones((6,6),np.uint8)) if enable else result
     #result=cv.erode(result,None,iterations=2) if erode else result
     result_erode=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
     cv.putText(result_erode,'erode enabled' if enable else 'erode disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/'+str(step)+'. erode.png',result_erode)
-    return result
+    cv.imwrite(path+str(step)+'. erode.png',result_erode)
+    return result,result_erode
 
 def dilate_image(result,step,enable):#dilate
-    global result_dilate
-    result=cv.dilate(result,np.ones((3,3),np.uint8)) if enable else result
+    result=cv.dilate(result,np.ones((5,5),np.uint8)) if enable else result
     #result=cv.dilate(result,None,iterations=1) if dilate else result
     result_dilate=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
     cv.putText(result_dilate,'dilate enabled' if enable else 'dilate disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/'+str(step)+'. dilate.png',result_dilate)
-    return result
+    cv.imwrite(path+str(step)+'. dilate.png',result_dilate)
+    return result,result_dilate
 
 def convert_binary(result,step,enable):#change to black & white
-    global result_threshold
     result=cv.threshold(result,127,255,cv.THRESH_BINARY)[1] if enable else result #85 #128
     result_threshold=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
     cv.putText(result_threshold,'threshold enabled' if enable else 'threshold disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/'+str(step)+'. threshold.png',result_threshold)
-    return result
+    cv.imwrite(path+str(step)+'. threshold.png',result_threshold)
+    return result,result_threshold
 
 def detect_edge(result,step,enable):#edge detection
-    global result_Canny
     result=cv.Canny(result,100,200,apertureSize=7) if enable else result #3,5,7
     result_Canny=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
     cv.putText(result_Canny,'Canny enabled' if enable else 'Canny disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite('image processing/'+str(step)+'. Canny.png',result_Canny)
-    return result
+    cv.imwrite(path+str(step)+'. Canny.png',result_Canny)
+    return result,result_Canny
 
 if __name__=='__main__':
     main()
