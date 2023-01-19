f64ae72e556d633c789e0527376727090d5c120f
dnshshkr <danish.shukor@live.iium.edu.my>
2023-01-19 10:31:28 +0800

    modified

--- imageprocessing.py
+++ imageprocessing.py
@@ -3,29 +3,42 @@ import numpy as np
 import os
 use_camera=1
 use_image=0
-folder_name='black'
-part_name='_black'
+folder_name='red'
+part_name='_'+folder_name
 save_path='image processing/'+folder_name+'/'
+
+imageX_name='image processing/red/1. imageX'
+imageY_name='image processing/red/2. imageY'
+imageXY_name='image processing/red/3. imageXY'
+caption_size=1
+
+#load image
+try:
+    imgX=cv.imread(imageX_name+'.png',cv.IMREAD_COLOR)
+    imgY=cv.imread(imageY_name+'.png',cv.IMREAD_COLOR)
+except:
+    pass
+imgXY=cv.imread(imageXY_name+'.png',cv.IMREAD_COLOR)
+
+final_result=imgXY.copy()
+result=cv.cvtColor(final_result,cv.COLOR_BGR2GRAY)
+
 for file_name in os.listdir(save_path):
     if os.path.isfile(save_path+file_name):
         os.remove(save_path+file_name)
-last_name='PhaseShifting'+part_name
-imageX_name='imageX_'+last_name
-imageY_name='imageY_'+last_name
-imageXY_name='imageXY_'+last_name
-caption_size=1
-imgX=cv.imread(imageX_name+'.png',cv.IMREAD_COLOR)
-imgY=cv.imread(imageY_name+'.png',cv.IMREAD_COLOR)
-imgXY=cv.imread(imageXY_name+'.png',cv.IMREAD_COLOR)
 
-final_result=imgY.copy()
-result=cv.cvtColor(final_result,cv.COLOR_BGR2GRAY)
-
-cv.putText(imgX,'imageX',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-cv.putText(imgY,'imageY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+#save path
+try:
+    cv.putText(imgX,'imageX',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(imgY,'imageY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+except:
+    pass
 cv.putText(imgXY,'imageXY',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-cv.imwrite(save_path+'1. imageX.png',imgX)
-cv.imwrite(save_path+'2. imageY.png',imgY)
+try:
+    cv.imwrite(save_path+'1. imageX.png',imgX)
+    cv.imwrite(save_path+'2. imageY.png',imgY)
+except:
+    pass
 cv.imwrite(save_path+'3. imageXY.png',imgXY)
 
 def process():
@@ -34,20 +47,22 @@ def main():
     global result
     process={1:smoothen_image,
              2:sharpen_image,
-             3:erode_image,
-             4:dilate_image,
-             5:laplacian,
-             6:convert_binary,
-             7:detect_edge}
+             3:equalize_background,
+             4:morphology,
+             5:erode_image,
+             6:dilate_image,
+             7:laplacian,
+             8:convert_binary,
+             9:detect_edge}
     
     stitch_image_array=[]
     result,stitch=process[1](result,4,0)
     stitch_image_array.append(stitch)
-    result,stitch=process[3](result,5,1)
+    result,stitch=process[3](result,5,0)
     stitch_image_array.append(stitch)
     result,stitch=process[4](result,6,1)
     stitch_image_array.append(stitch)
-    result,stitch=process[2](result,7,1)
+    result,stitch=process[2](result,7,0)
     stitch_image_array.append(stitch)
     result,stitch=process[5](result,8,0)
     stitch_image_array.append(stitch)
@@ -55,11 +70,19 @@ def main():
     stitch_image_array.append(stitch)
     result,stitch=process[7](result,10,0)
     stitch_image_array.append(stitch)
+    result,stitch=process[8](result,11,1)
+    stitch_image_array.append(stitch)
+    result,stitch=process[9](result,11,0)
+    stitch_image_array.append(stitch)
 
+    index=13
     #find contours
     contours=cv.findContours(result.copy(),cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)[0]
     contourImage=np.zeros((len(final_result),len(final_result[0]),3),np.uint8)
-    count_all=0
+    count_all=1
+    contour_label=0
+    contour_box=0
+    overlay_contours=1
     perimeter_min=8.0
     perimeter_max=200.0
     prevx,prevy=-1,-1
@@ -70,20 +93,31 @@ def main():
         #print('x: {}, y: {}'.format(x,y))
         #area=cv.contourArea(contour)
         if count_all:
-            cv.rectangle(final_result,(x,y),(x+w,y+h),(b,g,r),2)
-            cv.putText(final_result,"p{:.1f}".format(perimeter),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+            if contour_box:
+                cv.rectangle(final_result,(x,y),(x+w,y+h),(b,g,r),2)
+            if contour_label:
+                cv.putText(final_result,"p{:.1f}".format(perimeter),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
             cv.drawContours(contourImage,[contour],0,(b,g,r),1)
-            cv.putText(contourImage,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
-        elif (perimeter>=perimeter_min and perimeter<=perimeter_max) and (x!=prevx or y!=prevy):
-            cv.rectangle(final_result,(x,y),(x+w,y+h),(b,g,r),2)
-            cv.putText(final_result,"p{:.1f}".format(perimeter),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+            if overlay_contours:
+                cv.drawContours(final_result,[contour],0,(b,g,r),1)
+            if contour_label:
+                cv.putText(contourImage,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+        elif not count_all and (perimeter>=perimeter_min and perimeter<=perimeter_max) and (x!=prevx or y!=prevy):
+            if contour_box:
+                cv.rectangle(final_result,(x,y),(x+w,y+h),(b,g,r),2)
+            if contour_label:
+                cv.putText(final_result,"p{:.1f}".format(perimeter),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
+            cv.drawContours(contourImage,[contour],0,(b,g,r),1)
+            if overlay_contours:
+                cv.drawContours(final_result,[contour],0,(b,g,r),1)
+            if contour_label:
+                cv.putText(contourImage,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
             prevx,prevy=x,y
-            cv.drawContours(contourImage,[contour],0,(b,g,r),1)
-            cv.putText(contourImage,"p{}".format(i),(x,y-7),cv.FONT_HERSHEY_SIMPLEX,0.6,(b,g,r),2)
     cv.putText(contourImage,'contours',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite(save_path+'11. contours.png',contourImage)
+    cv.imwrite(save_path+str(index)+'. contours.png',contourImage)
+    index+=1
     cv.putText(final_result,'final result',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
-    cv.imwrite(save_path+'12. final result.png',final_result)
+    cv.imwrite(save_path+str(index)+'. final result.png',final_result)
 
     stitch_r0=np.concatenate((imgX,imgY,imgXY),axis=1)
     stitch_r1=np.concatenate((stitch_image_array[0],stitch_image_array[1],stitch_image_array[2]),axis=1)
@@ -96,7 +130,7 @@ def smoothen_image(result,step,enable):#filter noise
     result=cv.medianBlur(result,3) if enable else result
     #result=cv.blur(result,(9,9),0) if enable else result
     result_medianBlur=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_medianBlur,'medianBlur enabled' if enable else 'medianBlur disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_medianBlur,'medianBlur '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. medianBlur_'+str(enable)+'.png',result_medianBlur)
     return result,result_medianBlur
 
@@ -112,15 +146,37 @@ def sharpen_image(result,step,enable):#image sharpening
     # result=Image.filter(ImageFilter.SHARPEN)
 
     result_filter2D=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_filter2D,'filter2D enabled' if enable else 'filter2D disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_filter2D,'filter2D '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. filter2D_'+str(enable)+'.png',result_filter2D)
     return result,result_filter2D
+
+def equalize_background(result,step,enable):
+    if enable:
+        ycrcb=cv.cvtColor(cv.cvtColor(result,cv.COLOR_GRAY2BGR),cv.COLOR_BGR2YCrCb)
+        y,cr,cb=cv.split(ycrcb)
+        y_eq=cv.equalizeHist(y)
+        ycrcb_eq=cv.merge((y_eq,cr,cb))
+        result=cv.cvtColor(cv.cvtColor(ycrcb_eq,cv.COLOR_YCrCb2BGR),cv.COLOR_BGR2GRAY)
+    result_equalizeBackground=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
+    cv.putText(result_equalizeBackground,'equalize background '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.imwrite(save_path+str(step)+'. equalizeBackground_'+str(enable)+'.png',result_equalizeBackground)
+    return result,result_equalizeBackground
+
+def morphology(result,step,enable):
+    kernel=np.ones((5,5),np.uint8)
+    if enable:
+        closing=cv.morphologyEx(result,cv.MORPH_CLOSE,kernel)
+    result=cv.absdiff(result,closing) if enable else result
+    result_morphologicalClosing=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
+    cv.putText(result_morphologicalClosing,'morphological closing '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.imwrite(save_path+str(step)+'. morphologicalClosing_'+str(enable)+'.png',result_morphologicalClosing)
+    return result,result_morphologicalClosing
 
 def erode_image(result,step,enable):#erode
     #result=cv.erode(result,np.ones((6,6),np.uint8)) if enable else result
     result=cv.erode(result,None,iterations=2) if enable else result
     result_erode=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_erode,'erode enabled' if enable else 'erode disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_erode,'erode '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. erode_'+str(enable)+'.png',result_erode)
     return result,result_erode
 
@@ -128,28 +184,28 @@ def dilate_image(result,step,enable):#dilate
     #result=cv.dilate(result,np.ones((5,5),np.uint8)) if enable else result
     result=cv.dilate(result,None,iterations=1) if enable else result
     result_dilate=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_dilate,'dilate enabled' if enable else 'dilate disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_dilate,'dilate '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. dilate_'+str(enable)+'.png',result_dilate)
     return result,result_dilate
 
 def laplacian(result,step,enable):#laplacian
     result=cv.Laplacian(result,cv.CV_8U) if enable else result
     result_laplacian=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_laplacian,'Laplacian enabled' if enable else 'Laplacian disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_laplacian,'Laplacian '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. Laplacian_'+str(enable)+'.png',result_laplacian)
     return result,result_laplacian
 
 def convert_binary(result,step,enable):#change to black & white
-    result=cv.threshold(result,127,255,cv.THRESH_BINARY)[1] if enable else result #85 #128 #170
+    result=cv.threshold(result,20,255,cv.THRESH_BINARY)[1] if enable else result #85 #128 #170
     result_threshold=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_threshold,'threshold enabled' if enable else 'threshold disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_threshold,'threshold '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. threshold_'+str(enable)+'.png',result_threshold)
     return result,result_threshold
 
 def detect_edge(result,step,enable):#edge detection
     result=cv.Canny(result,100,200,apertureSize=7) if enable else result #3,5,7
     result_Canny=cv.cvtColor(result,cv.COLOR_GRAY2BGR)
-    cv.putText(result_Canny,'Canny enabled' if enable else 'Canny disabled',(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
+    cv.putText(result_Canny,'Canny '+('enabled' if enable else 'disabled'),(5,30),cv.FONT_HERSHEY_SIMPLEX,caption_size,(0,0,255),2)
     cv.imwrite(save_path+str(step)+'. Canny_'+str(enable)+'.png',result_Canny)
     return result,result_Canny
 
